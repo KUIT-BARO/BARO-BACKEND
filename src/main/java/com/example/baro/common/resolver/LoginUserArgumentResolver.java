079567f81f90bdfe12337b7c34643abc33b7f161
mr8356 <mr8356@naver.com>
2025-01-29 23:07:25 +0900

    LoginUser Session Argument resolve 구현 완료

--- src/main/java/com/example/baro/common/resolver/LoginUserArgumentResolver.java
+++ src/main/java/com/example/baro/common/resolver/LoginUserArgumentResolver.java
@@ -1,2 +1,29 @@
-package com.example.baro.common.resolver;public class LoginUserArgumentResolver {
+package com.example.baro.common.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		// @LoginUser 어노테이션이 붙은 파라미터만 처리
+		return parameter.getParameterAnnotation(LoginUser.class) != null;
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+								  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		HttpSession session = webRequest.getNativeRequest(HttpServletRequest.class).getSession(false);
+		if (session != null) {
+			return session.getAttribute("user");  // 세션에서 User 객체 가져오기
+		}
+		return null;
+	}
 }
