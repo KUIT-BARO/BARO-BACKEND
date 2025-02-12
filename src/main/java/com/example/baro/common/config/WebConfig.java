17a28a780dc6f9e2c4740603e6cf3e2ff3f35503
Shinjongyun <tigerjong123@gmail.com>
2025-02-12 23:36:24 +0900

    Fix: WebMvcConfigurer

--- src/main/java/com/example/baro/common/config/WebConfig.java
+++ src/main/java/com/example/baro/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.baro.common.config;
 import com.example.baro.common.resolver.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,5 +19,14 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(loginUserArgumentResolver);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**") // 모든 엔드포인트에 CORS 적용
+				.allowedOrigins("http://localhost:5173") // 프론트엔드 주소
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+				.allowedHeaders("*") // 모든 헤더 허용
+				.allowCredentials(true); // 인증 정보 포함 허용
 	}
 }
