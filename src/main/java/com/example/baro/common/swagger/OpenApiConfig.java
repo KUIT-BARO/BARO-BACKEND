6cd31d3b77e922a01f5331753fb5282f6a2e2af8
Shinjongyun <tigerjong123@gmail.com>
2025-02-22 13:46:28 +0900

    Fix: swagger 경로

--- src/main/java/com/example/baro/common/swagger/OpenApiConfig.java
+++ src/main/java/com/example/baro/common/swagger/OpenApiConfig.java
@@ -19,7 +19,7 @@ public class OpenApiConfig {
     @Bean
     public GroupedOpenApi v1Api() {
         // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
-        String[] paths = {"/api/v1/**"};
+        String[] paths = {"/**"};
 
         return GroupedOpenApi.builder()
                 .group("BARO API v1")  // 그룹 이름을 설정한다.
