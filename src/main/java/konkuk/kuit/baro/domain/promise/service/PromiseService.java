642cd9dff62ff5ddbb843890d9e51ffa070571d3
ksg <ksg001227@naver.com>
2025-03-04 14:51:18 +0900

    [Feat] #6 promiseService 구현

--- src/main/java/konkuk/kuit/baro/domain/promise/service/PromiseService.java
+++ src/main/java/konkuk/kuit/baro/domain/promise/service/PromiseService.java
@@ -1,2 +1,54 @@
-package konkuk.kuit.baro.domain.promise.service;public class PromiseService {
+package konkuk.kuit.baro.domain.promise.service;
+
+import konkuk.kuit.baro.domain.promise.dto.request.PromiseSuggestRequestDTO;
+import konkuk.kuit.baro.domain.promise.model.Promise;
+import konkuk.kuit.baro.domain.promise.model.PromiseMember;
+import konkuk.kuit.baro.domain.promise.repository.PromiseMemberRepository;
+import konkuk.kuit.baro.domain.promise.repository.PromiseRepository;
+import konkuk.kuit.baro.domain.user.model.User;
+import konkuk.kuit.baro.domain.user.repository.UserRepository;
+import konkuk.kuit.baro.global.common.exception.CustomException;
+import konkuk.kuit.baro.global.common.response.status.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PromiseService {
+
+    private final PromiseRepository promiseRepository;
+    private final PromiseMemberRepository promiseMemberRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void promiseSuggest(PromiseSuggestRequestDTO request, Long loginUserId) {
+
+        // 약속 엔티티 생성
+        Promise promise = Promise.builder()
+                .promiseName(request.getPromiseName())
+                .suggestedRegion(request.getSuggestedRegion())
+                .suggestedStartDate(request.getSuggestedStartDate())
+                .suggestedEndDate(request.getSuggestedEndDate())
+                .build();
+
+        // 약속 엔티티 저장
+        Promise savedPromise = promiseRepository.save(promise);
+
+        // 로그인한 유저 찾기
+        User loginUser = findLoginUser(loginUserId);
+
+        // 약속 참여자 엔티티 생성
+        PromiseMember promiseMember = PromiseMember.createPromiseMember(true, loginUser, savedPromise);
+
+        // 약속 참여자 엔티티 저장
+        promiseMemberRepository.save(promiseMember);
+    }
+
+    private User findLoginUser(Long loginUserId) {
+        return userRepository.findById(loginUserId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
 }
