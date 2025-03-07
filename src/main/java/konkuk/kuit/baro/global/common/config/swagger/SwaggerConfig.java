0c5f55e2ae1fe9ad95e076047afb8f1cb84a59a2
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-03-07 20:37:08 +0900

    [Feat] #8 Swagger 기본 설정

---
+++ src/main/java/konkuk/kuit/baro/global/common/config/swagger/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package konkuk.kuit.baro.global.common.config.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Baro 백엔드 API 명세서",
+                description = "Springdoc을 이용한 Swagger API 문서입니다.",
+                version = "1.0"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+        @Bean
+        public OpenAPI openAPI() {
+                return new OpenAPI()
+                        .components(new Components());
+        }
+}
