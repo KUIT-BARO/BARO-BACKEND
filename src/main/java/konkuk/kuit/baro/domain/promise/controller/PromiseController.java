f71d2c4a83937d03e21e092f83b6ae4c32da8ba4
ksg <ksg001227@naver.com>
2025-03-09 00:12:23 +0900

    [Feat] #18 약속 제안 관련 Swagger 명세 추가

--- src/main/java/konkuk/kuit/baro/domain/promise/controller/PromiseController.java
+++ src/main/java/konkuk/kuit/baro/domain/promise/controller/PromiseController.java
@@ -1,8 +1,12 @@
 package konkuk.kuit.baro.domain.promise.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import konkuk.kuit.baro.domain.promise.dto.request.PromiseSuggestRequestDTO;
 import konkuk.kuit.baro.domain.promise.service.PromiseService;
+import konkuk.kuit.baro.global.common.annotation.CustomExceptionDescription;
+import konkuk.kuit.baro.global.common.config.swagger.SwaggerResponseDescription;
 import konkuk.kuit.baro.global.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static konkuk.kuit.baro.global.common.config.swagger.SwaggerResponseDescription.*;
 
 @Slf4j
 @RestController
@@ -19,7 +25,10 @@ public class PromiseController {
 
     private final PromiseService promiseService;
 
+    @Tag(name = "약속 제안 API", description = "약속 제안 관련 API")
+    @Operation(summary = "약속 제안", description = "약속 이름, 날짜, 장소등을 입력하여 약속을 제안합니다.")
     @PostMapping
+    @CustomExceptionDescription(PROMISE_SUGGEST)
     public BaseResponse<Void> suggestPromise(@Valid @RequestBody PromiseSuggestRequestDTO request) {
         // 현재 로그인한 유저를 토큰에서 꺼낸 후, host 로써 약속 참여자 테이블에 저장해야함.
         // 아직 토큰 로직이 부재하기에 userId 1번을 집어넣음
--- src/main/java/konkuk/kuit/baro/global/common/response/BaseResponse.java
+++ src/main/java/konkuk/kuit/baro/global/common/response/BaseResponse.java
@@ -1,20 +1,26 @@
 package konkuk.kuit.baro.global.common.response;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @JsonPropertyOrder({"success", "code", "message", "data"})
 public class BaseResponse<T> {
 
     private final boolean success;
+
+    @Schema(example = "200")
     private final int code;
+
+    @Schema(example = "요청에 성공하였습니다.")
     private final String message;
     private final T data;
 
     private BaseResponse(T data) {
         this.success = true;
-        this.code = 200;
+        this.code = HttpStatus.OK.value();
         this.message = "요청에 성공하였습니다.";
         this.data = data;
     }
