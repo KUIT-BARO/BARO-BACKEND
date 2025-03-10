ebb73f885e7060d1215e15996bf648df670a55f1
Shinjongyun <tigerjong123@gmail.com>
2025-03-10 16:24:04 +0900

    [Feat] #14 AuthController 구현

---
+++ src/main/java/konkuk/kuit/baro/global/auth/controller/AuthController.java
@@ -0,0 +1,57 @@
+package konkuk.kuit.baro.global.auth.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import konkuk.kuit.baro.global.auth.jwt.service.JwtService;
+import konkuk.kuit.baro.global.auth.service.AuthService;
+import konkuk.kuit.baro.global.common.annotation.CustomExceptionDescription;
+import konkuk.kuit.baro.global.common.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Tag(name = "Auth", description = "Auth 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    private final JwtService jwtService;
+
+    @Operation(
+            summary = "로그인",
+            description = "로그인합니다. 토큰이 필요하지 않습니다."
+    )
+    @CustomExceptionDescription(LOGIN)
+    @PostMapping("/login")
+    public BaseResponse<LoginResponseDTO> login(@RequestBody LoginRequest request) {
+        return BaseResponse.ok(authService.login(request));
+    }
+
+    @Operation(
+            summary = "토큰 재발급",
+            description = "리프레시 토큰을 가지고 토큰을 재발급합니다. 리프레시 토큰이 필요합니다."
+    )
+    @CustomExceptionDescription(REISSUE)
+    @PostMapping("/reissue")
+    public BaseResponse<Void> reissueTokens(HttpServletRequest request, HttpServletResponse response) {
+        authService.reissueTokens(request, response);
+        return BaseResponse.ok(null);
+    }
+
+    @Operation(
+            summary = "로그아웃",
+            description = "로그아웃합니다. 액세스 토큰과 리프레시 토큰이 필요합니다."
+    )
+    @CustomExceptionDescription(LOGOUT)
+    @PatchMapping("/logout")
+    public BaseResponse<LogoutResponseDTO> logout(HttpServletRequest request) {
+        Optional<String> accessToken = jwtService.extractAccessToken(request);
+        Optional<String> refreshToken = jwtService.extractRefreshToken(request);
+        return BaseResponse.ok(authService.logout(accessToken, refreshToken));
+    }
+}
