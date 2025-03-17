66d19b3c388a6734b72d5d1bddbe7a1faa37caa4
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-03-17 17:29:32 +0900

    [Fix] #21 @Param import문 추가

--- src/main/java/konkuk/kuit/baro/domain/promise/repository/PromiseMemberRepository.java
+++ src/main/java/konkuk/kuit/baro/domain/promise/repository/PromiseMemberRepository.java
@@ -3,6 +3,7 @@ package konkuk.kuit.baro.domain.promise.repository;
 import konkuk.kuit.baro.domain.promise.model.PromiseMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
