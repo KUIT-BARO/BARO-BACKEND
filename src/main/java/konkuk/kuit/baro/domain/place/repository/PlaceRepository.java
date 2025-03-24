f1278ffeab5d5ca17b469d7138be657af09ed58a
ksg <ksg001227@naver.com>
2025-03-25 01:31:11 +0900

    [Feat] #29 PlaceRepository 구현

--- src/main/java/konkuk/kuit/baro/domain/place/repository/PlaceRepository.java
+++ src/main/java/konkuk/kuit/baro/domain/place/repository/PlaceRepository.java
@@ -1,9 +1,18 @@
 package konkuk.kuit.baro.domain.place.repository;
 
 import konkuk.kuit.baro.domain.place.model.Place;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+    @Query("SELECT p FROM Place p " +
+            "WHERE ST_Contains(ST_Buffer(:currentUserLocation, 2000), p.location) ")
+    List<Place> findByDistance(@Param("currentUserLocation") Point currentUserLocation);
 }
