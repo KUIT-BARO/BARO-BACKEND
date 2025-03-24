1106b5f1a480501be791f8b32a4bd4c74f4a9fad
Shinjongyun <tigerjong123@gmail.com>
2025-03-24 21:50:25 +0900

    [Feat] #28 ConfirmedPromiseResponseDTO 구현

---
+++ src/main/java/konkuk/kuit/baro/domain/promise/dto/response/ConfirmedPromiseResponseDTO.java
@@ -0,0 +1,31 @@
+package konkuk.kuit.baro.domain.promise.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfirmedPromiseResponseDTO {
+    @Schema(description = "약속 ID", example = "1")
+    private Long promiseId;
+
+    @Schema(description = "약속명", example = "BARO 회의")
+    private String promiseName;
+
+    @Schema(description = "확정 인원 이름", example = "김상균, 이정연, 신종윤")
+    private List<String> promiseMembersNames;
+
+    @Schema(description = "약속 확정된 장소명", example = "건대입구역")
+    private String placeName;
+
+    @Schema(description = "약속 확정된 날짜", example = "2025-01-01")
+    private LocalDate fixedDate;
+}
