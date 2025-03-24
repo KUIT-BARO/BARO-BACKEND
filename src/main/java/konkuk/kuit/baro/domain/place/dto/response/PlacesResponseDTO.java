62a13971767b8257726828d3092a62f6552c69e0
ksg <ksg001227@naver.com>
2025-03-25 01:31:56 +0900

    [Feat] #29 장소 조회 응답용 DTO 구현

--- src/main/java/konkuk/kuit/baro/domain/place/dto/response/PlacesResponseDTO.java
+++ src/main/java/konkuk/kuit/baro/domain/place/dto/response/PlacesResponseDTO.java
@@ -1,2 +1,21 @@
-package konkuk.kuit.baro.domain.place.dto.response;public class PlacesResponseDTO {
+package konkuk.kuit.baro.domain.place.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlacesResponseDTO {
+
+    @Schema(example = "1", description = "장소 ID")
+    private Long placeId;
+
+    @Schema(example = "37.5423265", description = "위도")
+    private Double latitude;
+
+    @Schema(example = "127.0759204", description = "경도")
+    private Double longitude;
 }
