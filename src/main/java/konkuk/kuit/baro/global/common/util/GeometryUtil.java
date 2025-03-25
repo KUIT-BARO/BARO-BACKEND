cf13eb762e2e95514bb4a49b0ff850c8058c9d54
ksg <ksg001227@naver.com>
2025-03-25 11:24:49 +0900

    [Refactor] #29 GeometryUtil 클래스 생성

---
+++ src/main/java/konkuk/kuit/baro/global/common/util/GeometryUtil.java
@@ -0,0 +1,15 @@
+package konkuk.kuit.baro.global.common.util;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
+
+public class GeometryUtil {
+
+    private static final GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
+
+    public static Point createPoint(Double latitude, Double longitude) {
+        return geometryFactory.createPoint(new Coordinate(longitude, latitude));
+    }
+}
