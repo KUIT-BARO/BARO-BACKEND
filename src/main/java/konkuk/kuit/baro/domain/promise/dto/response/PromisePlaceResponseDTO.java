d9aaf39daaf18babcf3853c432c27845b6bdd645
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-03-31 16:14:16 +0900

    [Fix] #37 약속 선택 초기 화면 DTO에 약속 참여자 정보도 포함

--- src/main/java/konkuk/kuit/baro/domain/promise/dto/response/PromisePlaceResponseDTO.java
+++ src/main/java/konkuk/kuit/baro/domain/promise/dto/response/PromisePlaceResponseDTO.java
@@ -1,20 +1,16 @@
 package konkuk.kuit.baro.domain.promise.dto.response;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import konkuk.kuit.baro.domain.place.dto.response.PlaceSearchResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class PromisePlaceResponseDTO {
-    @Schema(example = "1", description = "장소 ID")
-    private Long placeId;
-
-    @Schema(example = "37.5423265", description = "위도")
-    private Double latitude;
-
-    @Schema(example = "127.0759204", description = "경도")
-    private Double longitude;
+    List<PlaceSearchResponseDTO> places;
+    List<PromiseMemberDTO> members;
 }
