070ed270c297cc7a00b8bc486bb4f745d0512d29
Shinjongyun <tigerjong123@gmail.com>
2025-03-31 22:11:36 +0900

    [Feat] #39 핀 열람 로직 구현

--- src/main/java/konkuk/kuit/baro/domain/pin/controller/PinController.java
+++ src/main/java/konkuk/kuit/baro/domain/pin/controller/PinController.java
@@ -2,19 +2,12 @@ package konkuk.kuit.baro.domain.pin.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import konkuk.kuit.baro.domain.place.dto.request.PlaceSearchRequestDTO;
-import konkuk.kuit.baro.domain.place.dto.response.PlaceSearchResponseDTO;
-import konkuk.kuit.baro.domain.place.service.PlaceService;
-import konkuk.kuit.baro.global.common.annotation.CustomExceptionDescription;
+import konkuk.kuit.baro.domain.pin.service.PinService;
 import konkuk.kuit.baro.global.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static konkuk.kuit.baro.global.common.config.swagger.SwaggerResponseDescription.PLACE_SEARCH;
-
 
 @Tag(name = "핀 API", description = "핀 관련 API")
 @RestController
@@ -26,8 +19,8 @@ public class PinController {
 
     @Operation(summary = "핀 열람", description = "핀을 열람합니다.")
     @GetMapping("/{pinId}")
-    // @CustomExceptionDescription(PLACE_SEARCH)
-    public BaseResponse<List<PinResponseDTO>> placeSearch(@PathVariable Long pinId) {
+    // @CustomExceptionDescription(GET_PIN_DATA)
+    public BaseResponse<List<PinResponseDTO>> getPin(@PathVariable Long pinId) {
         return BaseResponse.ok(pinService.getPinData(pinId));
     }
 }
---
+++ src/main/java/konkuk/kuit/baro/domain/pin/service/PinService.java
@@ -0,0 +1,34 @@
+package konkuk.kuit.baro.domain.pin.service;
+
+import konkuk.kuit.baro.domain.category.repository.PlaceCategoryRepository;
+import konkuk.kuit.baro.domain.pin.model.Pin;
+import konkuk.kuit.baro.domain.pin.repository.PinRepository;
+import konkuk.kuit.baro.domain.place.repository.PlaceRepository;
+import konkuk.kuit.baro.global.common.exception.CustomException;
+import konkuk.kuit.baro.global.common.response.status.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PinService {
+
+    private final PinRepository pinRepository;
+
+    public PinResponseDTO getPinData(Long pinId){
+        Pin pin = pinRepository.findById(pinId)
+                .orElseThrow((() -> new CustomException(ErrorCode.PIN_NOT_FOUND))
+
+    return new PinResponseDTO(
+                pin.getUser().getName(),
+                pin.getUser().getEmail(),
+                pin.getUser().getProfileImage(),
+                pin.getReview(),
+                pin.getScore(),
+                pin.getPlace().getPlaceName()
+        );
+    }
+}
+
