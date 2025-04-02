fbb0f48e3e8f4f341b75bfdaef439164bdbeeeb4
ksg <ksg001227@naver.com>
2025-04-03 03:55:49 +0900

    [Feat] #43 약속 현황 - 투표중 API 를 위한 DTO 구현

---
+++ src/main/java/konkuk/kuit/baro/domain/promise/dto/response/PromiseStatusVotingPromiseResponseDTO.java
@@ -0,0 +1,22 @@
+package konkuk.kuit.baro.domain.promise.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PromiseStatusVotingPromiseResponseDTO {
+
+    @Schema(description = "약속 이름", example = "KUIT BARO 2차 회의")
+    private String promiseName;
+
+    @Schema(description = "투표를 종료할 수 있는지 여부", example = "true")
+    private Boolean canCloseVoting;
+
+    @Schema(description = "약속에 참여한 유저들의 투표 여부 관련 정보")
+    private List<PromiseMemberVoteStateDTO> promiseMemberVoteStateDTO;
+}
