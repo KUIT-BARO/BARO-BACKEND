169116c20866956e94cfc3b62caf6df90d8e6171
ksg <ksg001227@naver.com>
2025-04-04 00:46:39 +0900

    [Test] #50 투표 후보 목록 조회 API 테스트 구현

--- src/test/java/konkuk/kuit/baro/domain/promise/service/PromiseServiceTest.java
+++ src/test/java/konkuk/kuit/baro/domain/promise/service/PromiseServiceTest.java
@@ -6,12 +6,19 @@ import konkuk.kuit.baro.domain.place.model.Place;
 import konkuk.kuit.baro.domain.place.repository.PlaceRepository;
 import konkuk.kuit.baro.domain.promise.dto.response.ConfirmedPromiseResponseDTO;
 import konkuk.kuit.baro.domain.promise.dto.response.PromiseStatusConfirmedPromiseResponseDTO;
-import konkuk.kuit.baro.domain.promise.model.Promise;
-import konkuk.kuit.baro.domain.promise.repository.PromiseRepository;
+import konkuk.kuit.baro.domain.promise.model.*;
+import konkuk.kuit.baro.domain.promise.repository.*;
+import konkuk.kuit.baro.domain.user.model.User;
+import konkuk.kuit.baro.domain.user.repository.UserRepository;
+import konkuk.kuit.baro.domain.vote.model.PromisePlaceVoteHistory;
+import konkuk.kuit.baro.domain.vote.model.PromiseTimeVoteHistory;
 import konkuk.kuit.baro.domain.vote.model.PromiseVote;
+import konkuk.kuit.baro.domain.vote.repository.PromisePlaceVoteHistoryRepository;
+import konkuk.kuit.baro.domain.vote.repository.PromiseTimeVoteHistoryRepository;
 import konkuk.kuit.baro.domain.vote.repository.PromiseVoteRepository;
 import konkuk.kuit.baro.global.common.exception.CustomException;
 import konkuk.kuit.baro.global.common.response.status.ErrorCode;
+import konkuk.kuit.baro.global.common.util.DateUtil;
 import konkuk.kuit.baro.global.common.util.GeometryUtil;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -34,19 +41,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class PromiseServiceTest {
 
     @Autowired
+    PromiseMemberRepository promiseMemberRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
     private PromiseRepository promiseRepository;
     @Autowired
     private PlaceRepository placeRepository;
+    @Autowired
+    PromiseSuggestedPlaceRepository promiseSuggestedPlaceRepository;
+    @Autowired
+    PromiseCandidatePlaceRepository promiseCandidatePlaceRepository;
+    @Autowired
+    PromiseCandidateTimeRepository promiseCandidateTimeRepository;
+    @Autowired
+    PromiseTimeVoteHistoryRepository promiseTimeVoteHistoryRepository;
+    @Autowired
+    PromisePlaceVoteHistoryRepository promisePlaceVoteHistoryRepository;
 
-    @Autowired private PromiseService promiseService;
-    @Autowired private PromiseVoteRepository promiseVoteRepository;
+    @Autowired
+    private PromiseService promiseService;
+    @Autowired
+    private PromiseVoteRepository promiseVoteRepository;
 
     @PersistenceContext
     private EntityManager em;
 
     @Test
     @DisplayName("약속 현황 - 확정 테스트")
-    @Rollback(value = false)
     void confirm() {
         // given
         Promise promise = Promise.builder()
@@ -222,5 +244,103 @@ class PromiseServiceTest {
                 .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.TIME_EXCEED.getMessage());
     }
+
+    @Test
+    @DisplayName("투표 후보 목록 조회 테스트")
+    void getVoteCandidateList() {
+        // given: 사용자 및 약속 생성
+        User user = User.builder()
+                .email("hong@konkuk.ac.kr")
+                .name("홍길동")
+                .password("qwer1234!")
+                .profileImage("image.png")
+                .build();
+        userRepository.save(user);
+
+        Promise promise = Promise.builder()
+                .promiseName("컴퓨터 공학부 개강파티")
+                .suggestedRegion("지그재그")
+                .suggestedStartDate(LocalDate.now().minusDays(1))
+                .suggestedEndDate(LocalDate.now().plusDays(1))
+                .build();
+        promiseRepository.save(promise);
+
+        em.flush();
+        em.clear();
+
+        // given: 약속 멤버 생성
+        User findUser = userRepository.findById(1L).get();
+        Promise findPromise = promiseRepository.findById(1L).get();
+
+        PromiseMember promiseMember = PromiseMember.createPromiseMember(true, "#F4F4F4", findUser, findPromise);
+        promiseMemberRepository.save(promiseMember);
+
+        // given: 장소 및 제안된 장소 생성
+        Place place = Place.builder()
+                .placeName("스타벅스 건대점")
+                .location(GeometryUtil.createPoint(37.7749295, 122.4194155))
+                .placeAddress("광진구 화양동")
+                .build();
+        placeRepository.save(place);
+
+        em.flush();
+        em.clear();
+
+        // given: 투표 데이터 생성
+        PromiseMember findPromiseMember = promiseMemberRepository.findById(1L).get();
+        Place findPlace = placeRepository.findById(1L).orElseThrow();
+
+        PromiseSuggestedPlace promiseSuggestedPlace = PromiseSuggestedPlace.createPromiseSuggestedPlace(findPromiseMember, findPlace);
+        promiseSuggestedPlaceRepository.save(promiseSuggestedPlace);
+
+        // given: 약속 투표 생성
+        PromiseVote promiseVote = PromiseVote.builder()
+                .voteEndTime(LocalDateTime.now())
+                .build();
+        promiseVoteRepository.save(promiseVote);
+
+        findPromise = promiseRepository.findById(1L).get();
+        findPromise.setPromiseVote(promiseVote);
+        em.flush();
+        em.clear();
+
+        // given: 투표 후보 장소 및 시간 생성
+        PromiseVote findPromiseVote = promiseVoteRepository.findById(1L).get();
+        findPlace = placeRepository.findById(1L).get();
+        PromiseCandidatePlace promiseCandidatePlace = PromiseCandidatePlace.createPromiseCandidatePlace(findPromiseVote, findPlace);
+        promiseCandidatePlaceRepository.save(promiseCandidatePlace);
+
+        PromiseCandidateTime promiseCandidateTime = PromiseCandidateTime.createPromiseCandidateTime(
+                LocalDate.of(2025, 7, 3), LocalTime.now(), findPromiseVote);
+        promiseCandidateTimeRepository.save(promiseCandidateTime);
+
+        em.flush();
+        em.clear();
+
+        // when: 사용자가 실제로 투표한 기록 추가
+        PromiseCandidateTime findPromiseCandidateTime = promiseCandidateTimeRepository.findById(1L).get();
+        PromiseCandidatePlace findPromiseCandidatePlace = promiseCandidatePlaceRepository.findById(1L).get();
+        findPromiseVote = promiseVoteRepository.findById(1L).get();
+        findPromiseMember = promiseMemberRepository.findById(1L).get();
+
+        PromiseTimeVoteHistory promiseTimeVoteHistory = PromiseTimeVoteHistory.createPromiseTimeVoteHistory(
+                findPromiseVote, findPromiseCandidateTime, findPromiseMember);
+        promiseTimeVoteHistoryRepository.save(promiseTimeVoteHistory);
+
+        PromisePlaceVoteHistory promisePlaceVoteHistory = PromisePlaceVoteHistory.createPromisePlaceVoteHistory(
+                findPromiseCandidatePlace, findPromiseVote, findPromiseMember);
+        promisePlaceVoteHistoryRepository.save(promisePlaceVoteHistory);
+
+        em.flush();
+        em.clear();
+
+        assertThat(promiseService.getVoteCandidateList(1L, 1L, true).getCandidateTimes().get(0).getIsSelected()).isTrue();
+        assertThat(promiseService.getVoteCandidateList(1L, 1L, true).getCandidateTimes().get(0).getDate()).isEqualTo(DateUtil.formatDate(LocalDate.of(2025, 7, 3)));
+
+        assertThat(promiseService.getVoteCandidateList(1L, 1L, true).getCandidatePlaces().get(0).getIsSelected()).isTrue();
+        assertThat(promiseService.getVoteCandidateList(1L, 1L, true).getCandidatePlaces().get(0).getPlaceName()).isEqualTo("스타벅스 건대점");
+
+    }
+
 
 }
