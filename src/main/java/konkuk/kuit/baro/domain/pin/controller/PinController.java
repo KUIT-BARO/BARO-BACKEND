8331f43fa18673a82ced4e465f3ea895201d6e30
Shinjongyun <tigerjong123@gmail.com>
2025-04-06 23:06:21 +0900

    [Feat] #39 SwaggerDescription 추가

--- src/main/java/konkuk/kuit/baro/domain/pin/controller/PinController.java
+++ src/main/java/konkuk/kuit/baro/domain/pin/controller/PinController.java
@@ -7,9 +7,14 @@ import konkuk.kuit.baro.domain.pin.dto.request.PinRequestDTO;
 import konkuk.kuit.baro.domain.pin.dto.response.PinResponseDTO;
 import konkuk.kuit.baro.domain.pin.service.PinService;
 import konkuk.kuit.baro.global.auth.resolver.CurrentUserId;
+import konkuk.kuit.baro.global.common.annotation.CustomExceptionDescription;
 import konkuk.kuit.baro.global.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import static konkuk.kuit.baro.global.common.config.swagger.SwaggerResponseDescription.PIN_VIEW;
+import static konkuk.kuit.baro.global.common.config.swagger.SwaggerResponseDescription.PIN_REGISTER;
+
 
 @Tag(name = "핀 API", description = "핀 관련 API")
 @RestController
@@ -21,14 +26,14 @@ public class PinController {
 
     @Operation(summary = "핀 열람", description = "핀을 열람합니다.")
     @GetMapping("/{pinId}")
-    // @CustomExceptionDescription(PIN_VIEW)
+    @CustomExceptionDescription(PIN_VIEW)
     public BaseResponse<PinResponseDTO> getPin(@PathVariable Long pinId) {
         return BaseResponse.ok(pinService.getPinData(pinId));
     }
 
     @Operation(summary = "핀 등록", description = "새로운 핀을 등록합니다.")
     @PostMapping
-    // @CustomExceptionDescription(PIN_REGISTER)
+    @CustomExceptionDescription(PIN_REGISTER)
     public BaseResponse<Void> registerPin(@RequestBody @Valid PinRequestDTO request,
                                           @CurrentUserId Long userId) {
         pinService.registerPinData(request, userId);
