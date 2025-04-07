345f845001059248732f1d22f26263fd83bc7298
ksg <ksg001227@naver.com>
2025-04-07 22:48:25 +0900

    [Feat] #52 투표 종료 API 를 위한 DB 조회 로직 구현

--- src/main/java/konkuk/kuit/baro/domain/vote/repository/PromisePlaceVoteHistoryRepository.java
+++ src/main/java/konkuk/kuit/baro/domain/vote/repository/PromisePlaceVoteHistoryRepository.java
@@ -1,12 +1,28 @@
 package konkuk.kuit.baro.domain.vote.repository;
 
+import konkuk.kuit.baro.domain.promise.model.PromiseCandidatePlace;
 import konkuk.kuit.baro.domain.promise.model.PromiseMember;
 import konkuk.kuit.baro.domain.vote.model.PromisePlaceVoteHistory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PromisePlaceVoteHistoryRepository extends JpaRepository<PromisePlaceVoteHistory, Long> {
 
     void deleteAllByPromiseMember(PromiseMember promiseMember);
+
+    // 약속 장소 투표 내역을 확인하여, 가장 많이 투표된 약속 후보 시간별로 내림차순 정렬하여 반환
+    @Query("""
+            SELECT ppvh.promiseCandidatePlace
+            FROM PromisePlaceVoteHistory ppvh
+            WHERE ppvh.promiseVote.id = :promiseVoteId
+            GROUP BY ppvh.promiseCandidatePlace
+            ORDER BY COUNT(ppvh) DESC
+            """)
+    List<PromiseCandidatePlace> findMostVotedCandidatePlace(@Param("promiseVoteId") Long promiseVoteId, Pageable pageable);
 }
--- src/main/java/konkuk/kuit/baro/domain/vote/repository/PromiseTimeVoteHistoryRepository.java
+++ src/main/java/konkuk/kuit/baro/domain/vote/repository/PromiseTimeVoteHistoryRepository.java
@@ -1,10 +1,16 @@
 package konkuk.kuit.baro.domain.vote.repository;
 
+import konkuk.kuit.baro.domain.promise.model.PromiseCandidateTime;
 import konkuk.kuit.baro.domain.promise.model.PromiseMember;
 import konkuk.kuit.baro.domain.vote.model.PromiseTimeVoteHistory;
 import konkuk.kuit.baro.domain.vote.model.PromiseVote;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PromiseTimeVoteHistoryRepository extends JpaRepository<PromiseTimeVoteHistory, Long> {
@@ -14,4 +20,14 @@ public interface PromiseTimeVoteHistoryRepository extends JpaRepository<PromiseT
     boolean existsByPromiseVoteAndPromiseMember(PromiseVote promiseVote, PromiseMember promiseMember);
 
     void deleteAllByPromiseMember(PromiseMember promiseMember);
+
+    // 약속 시간 투표 내역을 확인하여, 가장 많이 투표된 약속 후보 시간별로 내림차순 정렬하여 반환
+    @Query("""
+            SELECT ptvh.promiseCandidateTime
+            FROM PromiseTimeVoteHistory ptvh
+            WHERE ptvh.promiseVote.id = :promiseVoteId
+            GROUP BY ptvh.promiseCandidateTime
+            ORDER BY COUNT(ptvh) DESC
+            """)
+    List<PromiseCandidateTime> findMostVotedCandidateTime(@Param("promiseVoteId") Long promiseVoteId, Pageable pageable);
 }
