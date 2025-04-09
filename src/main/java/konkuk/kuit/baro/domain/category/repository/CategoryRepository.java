731a948f0a465b211f3bc62c8d421b6da3d800af
Shinjongyun <tigerjong123@gmail.com>
2025-04-09 21:41:19 +0900

    [Feat] #39 카테고리 이름으로 조회 가능한 메서드 추가

--- src/main/java/konkuk/kuit/baro/domain/category/repository/CategoryRepository.java
+++ src/main/java/konkuk/kuit/baro/domain/category/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ import konkuk.kuit.baro.domain.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByCategoryNameIn(List<String> names);
 }
