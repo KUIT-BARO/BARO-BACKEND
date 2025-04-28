34d9f0e6eeebf4b802411542b3b372c961bb3994
Shinjongyun <tigerjong123@gmail.com>
2025-04-28 17:29:03 +0900

    [Fix] #39 PinRequestDTO placeID로 수정

--- src/main/java/konkuk/kuit/baro/domain/pin/dto/request/PinRequestDTO.java
+++ src/main/java/konkuk/kuit/baro/domain/pin/dto/request/PinRequestDTO.java
@@ -21,25 +21,9 @@ import java.util.List;
 @ParameterObject
 public class PinRequestDTO {
 
-    @JsonProperty("PlaceName")
-    @NotNull(message = "핀 등록 장소 이름은 비어있을 수 없습니다.")
-    private String placeName;
-
-    @JsonProperty("PlaceAddress")
-    @NotNull(message = "핀 등록 장소 주소는 비어있을 수 없습니다.")
-    private String placeAddress;
-
-    @JsonProperty("latitude")
-    @NotNull(message = "latitude 값이 존재하지 않습니다.")
-    @Min(value = -90, message = "latitude는 -90 이상이어야합니다.")
-    @Max(value = 90, message = "latitude는 90 이하여야합니다.")
-    private Double latitude;
-
-    @JsonProperty("longitude")
-    @NotNull(message = "longitude 값이 존재하지 않습니다.")
-    @Min(value = -180, message = "longitude는 -180 이상이어야합니다.")
-    @Max(value = 180, message = "longitude는 180 이하여야합니다.")
-    private Double longitude;
+    @JsonProperty("PlaceId")
+    @NotNull(message = "핀 등록 장소는 비어있을 수 없습니다.")
+    private long placeId;
 
     @JsonProperty("review")
     @NotBlank(message = "리뷰 내용은 비어있을 수 없습니다.")
